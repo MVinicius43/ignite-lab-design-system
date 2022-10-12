84b5227d4211fa8ee3a0285ad4feb0bb12e732e1
MVinicius43 <marcos.silva@psvsistemas.com.br>
2022-10-12 19:29:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+}
